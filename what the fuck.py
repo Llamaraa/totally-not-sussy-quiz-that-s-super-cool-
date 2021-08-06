e469286ea2ff68c2143909dbf94f3cd560a821b2
weegeedoescoding <67148946+weegeedoescoding@users.noreply.github.com>
2021-08-07 00:00:19 +0100

    uhhh... poop
    added the file

---
+++ what the fuck.py
@@ -0,0 +1,137 @@
+print("Cool totally not sussy quiz!")
+question1 = "When was Google launched?"
+options1 = "a. 2234\nb. 1992\nc. When yo mama started complaining to the manager\nd. 1998\n"
+print(question1)
+print(options1)
+
+while True:
+    response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+    if response == "d":
+        break
+    else:
+        print("WRONG! Try again.")
+
+        while True:
+            response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+            if response == "d":
+                stop = True
+                break
+            else:
+                print("lol you're a noob at this get gud")
+                stop = True
+                break
+        if stop:
+            break
+
+# DO the same for the next questions of your round (copy-paste-copy-paste).
+# At the end of the round, paste the following code for the bonus question.
+
+# Now the program will ask the user to go for the bonus question or not
+
+while True:
+    bonus = input("Another go, eh?\nHit 'y' for yes and 'n' for no.\n")
+
+    if bonus == "y":
+        print("What specific chocolate tastes like puke?")
+        print("a. The generic chocolate you get at a shop\nb. Cadbury's\nc. Hershey's\nd. Milka")
+
+        while True:
+            response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+            if response == "c":
+                break
+            else:
+                print("Get gud, n00b!")
+
+            while True:
+                response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+
+                if response == "c":
+                    stop = True
+                    break
+                else:
+                    print("man you're total garbage at this, you got an IQ of -5 or some shit?")
+                    stop = True
+                    break
+            if stop:
+                break
+        break
+    elif bonus == "n":
+        break
+    else:
+        print("INVALID INPUT!!! Only hit 'y' or 'n' for your response")
+
+        
+while True:
+    bonus = input("Wanna do some more?\nHit 'y' for yes and 'n' for no.\n")
+
+    if bonus == "y":
+        print("Who's joe?")
+        print("a. Me\nb. Mama\nc. Gay\nd. Pussy")
+
+        while True:
+            response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+            if response == "b":
+                break
+            else:
+                print("You suck.")
+
+            while True:
+                response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+
+                if response == "b":
+                    stop = True
+                    break
+                else:
+                    print("Sorry, outta tries.")
+                    stop = True
+                    break
+            if stop:
+                break
+        break
+    elif bonus == "n":
+        break
+    else:
+        print("INVALID INPUT!!! Only hit 'y' or 'n' for your response")
+
+                
+while True:
+    bonus = input("One more, whatddya say?\nHit 'y' for yes and 'n' for no.\n")
+
+    if bonus == "y":
+        print("Who bit the dust?")
+        print("a. Amother one\nb. Your dad, right after he went to get milk\nc. The dust\nd. bananaslamma")
+
+        while True:
+            response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+            if response == "a":
+                break
+            else:
+                print("mmm monke.")
+
+            while True:
+                response = input("Hit 'a', 'b', 'c' or 'd' for your answer\n")
+
+
+                if response == "a":
+                    stop = True
+                    break
+                else:
+                    print("pwned!")
+                    stop = True
+                    break
+            if stop:
+                break
+        break
+    elif bonus == "n":
+        break
+    else:
+        print("INVALID INPUT!!! Only hit 'y' or 'n' for your response")
+
+# Now do the same as done above for the next round and another bonus question.
